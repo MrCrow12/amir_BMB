67bceb0a1154ad54d84e097c742e7f3b6358a2d7
MrCrow12 <109727888+MrCrow12@users.noreply.github.com>
2024-12-27 20:42:51 +0330

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(
-    name="amir_sms_bomber",
-    version="1.0",
+    name='amir_sms_bomber',
+    version='1.0',
     packages=find_packages(),
-    description="please do not try on others.",
-    author="amirB",
-    author_email="amirrobot64@gmail.com",
-    url="https://github.com/MrCrow12/amir_BMB",
+    install_requires=[
+        'requests',
+    ],
+    author='amir',
+    author_email='amirrobot64@gmail.com',
+    description='please do not try on others',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/MrCrow12/amir_BMB', 
 )
