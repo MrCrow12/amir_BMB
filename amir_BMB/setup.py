5497ae84375768bce80e0b2a518957e90953a634
MrCrow12 <109727888+MrCrow12@users.noreply.github.com>
2024-12-27 19:21:44 +0330

    Update setup.py

--- amir_BMB/setup.py
+++ amir_BMB/setup.py
@@ -6,6 +6,6 @@ setup(
     packages=find_packages(),
     description="please do not try on others.",
     author="amirB",
-    author_email="amirrobot64@gmail.com"
-    url=
+    author_email="amirrobot64@gmail.com",
+    url="https://github.com/MrCrow12/amir_BMB",
 )
