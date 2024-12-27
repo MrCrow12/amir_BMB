c1ab928f838a07f14514af551238c8b7b6ade6e2
MrCrow12 <109727888+MrCrow12@users.noreply.github.com>
2024-12-27 19:19:45 +0330

    Add files via upload

---
+++ amir_BMB/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup,find_packages
+
+setup(
+    name="amir_sms_bomber",
+    version="1.0",
+    packages=find_packages(),
+    description="please do not try on others.",
+    author="amirB",
+    author_email="amirrobot64@gmail.com"
+    url=
+)
